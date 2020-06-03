879967d65429fe081d123d22f2783326e69d6333
Kira Slobodina <slobodina.kira@gmail.com>
2020-06-03 20:36:16 +0100

    Add description below the title

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 function App() {
   return (
-    <nav className="d-flex justify-content-center">
-      <a href="#" className="h1 my-4 text-primary">
-        React Recipes
-      </a>
-    </nav>
+    <>
+      <nav className="d-flex justify-content-center">
+        <a href="#" className="h1 my-4 text-primary">
+          React Recipes
+        </a>
+      </nav>
+      <p className="text-center mb-4">Browse all the recipes here, select a category or search for an ingredient.</p>
+    </>
   )
 }
 
