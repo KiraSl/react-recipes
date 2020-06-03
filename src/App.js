c3eae581bb5cdd78e7befe1be81ce4553a8d4d3a
Kira Slobodina <slobodina.kira@gmail.com>
2020-06-03 21:38:20 +0100

    Add a row with first cards and make it responsive

--- src/App.js
+++ src/App.js
@@ -8,7 +8,47 @@ function App() {
           React Recipes
         </a>
       </nav>
-      <p className="text-center mb-4">Browse all the recipes here, select a category or search for an ingredient.</p>
+      <div className="container">
+        <p className="text-center mb-4">Browse all the recipes here, select a category or search for an ingredient.</p>
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 mb-4">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-lg-4 mb-4">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-lg-4 mb-4">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-lg-4 mb-4">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
