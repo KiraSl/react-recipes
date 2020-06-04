5a015a5f43e6e86843d23941db5e8d2373f4a513
Kira Slobodina <42243887+KiraSl@users.noreply.github.com>
2020-06-04 23:16:04 +0100

    Make App a class component, fetch and console log the recipes (#15)
    * Make App a class component, fetch and console log the recipes
    
    * Remove extra empty line
    
    Co-authored-by: Sam De Maeyer <sam_demaeyer@hotmail.com>
    
    Co-authored-by: Sam De Maeyer <sam_demaeyer@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,56 +1,73 @@
 import React from 'react'
 
-function App() {
-  return (
-    <>
-      <nav className="d-flex justify-content-center">
-        <a href="#" className="h1 my-4 text-primary">
-          React Recipes
-        </a>
-      </nav>
-      <div className="container">
-        <p className="text-center mb-4">Browse all the recipes here, select a category or search for an ingredient.</p>
-        <div className="row">
-          <div className="col-sm-6 col-lg-4 mb-4">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      recipes: [],
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch('/api/recipes/all.json')
+    const recipes = await response.json()
+    this.setState({ recipes })
+  }
+
+  render() {
+    console.log(this.state.recipes)
+    return (
+      <>
+        <nav className="d-flex justify-content-center">
+          <a href="#" className="h1 my-4 text-primary">
+            React Recipes
+          </a>
+        </nav>
+        <div className="container">
+          <p className="text-center mb-4">Browse all the recipes here, select a category or search for an ingredient.</p>
+          <div className="row">
+            <div className="col-sm-6 col-lg-4 mb-4">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 col-lg-4 mb-4">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <div className="col-sm-6 col-lg-4 mb-4">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 col-lg-4 mb-4">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <div className="col-sm-6 col-lg-4 mb-4">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 col-lg-4 mb-4">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <div className="col-sm-6 col-lg-4 mb-4">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default App
