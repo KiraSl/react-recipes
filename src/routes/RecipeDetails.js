d84367d8d0f51b4f6da04804a1dddfdc838182d1
Kira Slobodina <42243887+KiraSl@users.noreply.github.com>
2020-06-13 11:20:17 +0100

    Display details of a single recipe (#20)
    * Display details of a single recipe
    
    * CR/ Rename some variables and remove extra css classes
    
    * CR/ Remove w-100 and move 'text-center' class on the wrapping div
    
    * Use Optional chaining

--- src/routes/RecipeDetails.js
+++ src/routes/RecipeDetails.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const RecipeDetails = props => {
-  const { id } = props.match.params
-  
-  return (
-    <h1 className="text-center">{id}</h1>
-  )
+class RecipeDetails extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      recipe: {},
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`/api/recipes/${this.props.match.params.id}.json`)
+    const recipe = await response.json()
+    this.setState({ recipe })
+  }
+
+  render() {
+    const { recipe, recipe: {recipeDetail} } = this.state
+
+    return (
+      <div className="text-center">
+        <h2 className="text-secondary text-uppercase mb-4">{recipe.title}</h2>
+        <p>{recipe.description}</p>
+        <img
+          src={recipeDetail?.image}
+          alt={recipe.title}
+          className="pb-4"
+        />
+        <div className="text-left border-bottom pb-4">
+          {recipeDetail?.description}
+        </div>
+      </div>
+    )
+  }
 }
 
 RecipeDetails.propTypes = {
