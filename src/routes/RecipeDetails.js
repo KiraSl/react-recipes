83444a19911661f8fea9fd6adbcb7302ddc118a6
Kira Slobodina <42243887+KiraSl@users.noreply.github.com>
2020-06-15 20:54:20 +0100

    Render ingredients and preparation steps (#22)
    * Render ingredients and preparation steps
    
    * Add the key, remove text-left and change margin

--- src/routes/RecipeDetails.js
+++ src/routes/RecipeDetails.js
@@ -20,21 +20,41 @@ class RecipeDetails extends React.Component {
     const { recipe, recipe: {recipeDetail} } = this.state
 
     return (
-      <div className="text-center">
-        <h2 className="text-secondary text-uppercase mb-4">{recipe.title}</h2>
-        <p>{recipe.description}</p>
-        <img
-          src={recipeDetail?.image}
-          alt={recipe.title}
-          className="pb-4"
-        />
+      <>
+        <div className="text-center">
+          <h2 className="text-secondary text-uppercase mb-4">{recipe.title}</h2>
+          <p>{recipe.description}</p>
+          <img
+            src={recipeDetail?.image}
+            alt={recipe.title}
+            className="pb-4"
+          />
+        </div>
         <div 
-          className="text-left border-bottom pb-4" 
+          className="border-bottom pb-4"
           dangerouslySetInnerHTML={
             {__html: recipeDetail?.description}
           }>
         </div>
-      </div>
+        <h6 className="text-secondary text-uppercase my-4">{recipe.title}</h6>
+        <div className="row">
+          <div className="col-md-4">
+            <p className="font-italic ">
+              Serves {recipeDetail?.serves}
+            </p>
+            <ul className="list-unstyled">
+              {recipeDetail?.ingredients.map((ingredient, index) => (
+                <li className="font-weight-bold" key={index}>{ingredient}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-md-8">
+            {recipeDetail?.preparationSteps.map((step, index) => (
+              <p key={index}>{step}</p>
+            ))}
+          </div>
+        </div>
+      </>
     )
   }
 }
