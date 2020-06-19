8230a8e5c61702dd79ed4f45c489d172c5538354
Kira Slobodina <slobodina.kira@gmail.com>
2020-06-19 14:04:31 +0100

    Link main page with recipes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { HomePage } from './routes/HomePage'
 import { RecipeDetails } from './routes/RecipeDetails'
 
@@ -7,18 +7,17 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <nav className="d-flex justify-content-center">
-          <a href="#" className="h1 my-4 text-primary">
-            React Recipes
-          </a>
-        </nav>
-        <div className="container">
-          <Router>
+        <Router>
+          <nav className="d-flex justify-content-center">
+            <Link to="/" className="h1 my-4 text-primary">
+              React Recipes
+            </Link>
+          </nav>
+          <div className="container">
             <Route exact path="/" component={HomePage} />
-            <Route exact path="/recipe/:id" render={routeProps => (<RecipeDetails {...routeProps}/>)}>
-            </Route>
-          </Router>
-        </div>
+            <Route exact path="/recipe/:id" render={routeProps => (<RecipeDetails {...routeProps} />)} />
+          </div>
+        </Router>
       </>
     )
   }
