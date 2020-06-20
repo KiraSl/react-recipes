098a748bcdb4fd24d7b8f05e3fdc9fb3e64f493c
Kira Slobodina <slobodina.kira@gmail.com>
2020-06-20 11:08:05 +0100

    Updates

---
+++ precache-manifest.75220c5a42a77c0db42a43610da7cc2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887b983ed0414fd8675f0b1525ce57a6",
+    "url": "/react-recipes/index.html"
+  },
+  {
+    "revision": "b7b4862f8ade23bef138",
+    "url": "/react-recipes/static/css/main.ade98a12.chunk.css"
+  },
+  {
+    "revision": "ce3954dfe1c8f9d5ae78",
+    "url": "/react-recipes/static/js/2.e4c2930c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-recipes/static/js/2.e4c2930c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b4862f8ade23bef138",
+    "url": "/react-recipes/static/js/main.8e8583ef.chunk.js"
+  },
+  {
+    "revision": "8488a1054e81264ffede",
+    "url": "/react-recipes/static/js/runtime-main.ce7d2d65.js"
+  }
+]);
