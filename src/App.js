e3b49530fa7ce41806ef27ae49be103efadc953c
Kira Slobodina <slobodina.kira@gmail.com>
2020-06-20 10:52:35 +0100

    CR/ Remove wrapping  empty tag

--- src/App.js
+++ src/App.js
@@ -6,19 +6,17 @@ import { RecipeDetails } from './routes/RecipeDetails'
 class App extends React.Component {
   render() {
     return (
-      <>
-        <Router>
-          <nav className="d-flex justify-content-center">
-            <Link to="/" className="h1 my-4 text-primary">
-              React Recipes
-            </Link>
-          </nav>
-          <div className="container">
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/recipe/:id" render={routeProps => (<RecipeDetails {...routeProps} />)} />
-          </div>
-        </Router>
-      </>
+      <Router>
+        <nav className="d-flex justify-content-center">
+          <Link to="/" className="h1 my-4 text-primary">
+            React Recipes
+          </Link>
+        </nav>
+        <div className="container">
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/recipe/:id" render={routeProps => (<RecipeDetails {...routeProps} />)} />
+        </div>
+      </Router>
     )
   }
 }
